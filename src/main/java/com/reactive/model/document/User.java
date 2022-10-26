54e799fc158b0a3deb88d0390e1b183cf0d9c8f1
namichetti <nestor.amichetti@gmail.com>
2022-10-26 12:29:24 -0300

    Modifico la clase User

--- src/main/java/com/reactive/model/document/User.java
+++ src/main/java/com/reactive/model/document/User.java
@@ -1,16 +1,13 @@
 package com.reactive.model.document;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
 @Builder
 @Getter
-@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 @Document(collection = "users")
 public class User {
